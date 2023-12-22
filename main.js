4895884158930d907928f86099b56f5f38dd0cf1
Ramon Cortez <146107835+Rcortez199325@users.noreply.github.com>
2023-12-22 14:26:49 -0700

    Update main.js
    12-22-2023
    version 0.01

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-//Archivo JS de pruebas
+//Desde el inicio se crea un estado de "pendiente" donde:
+//Esta esperando a que se ingresen los digitos 1-9 y . ( en caso de que empiece con '.' mantener el cero inicial, para el primer numero) y 0-9 para del segundo en delante o 
+//Esta esperando a que se ingrese la operacion (-) por si se va a realizar una operacion con numero negativo.
+//
+
+//Prevent default sobre los botones (evita que se refresque la pagina)
+window.onload = ()=>{
+    const botones = document.querySelectorAll(".digit");
+    for(let i=0;i<botones.length;i++) {
+        botones[i].event.preventDefault();
+    }
+};
